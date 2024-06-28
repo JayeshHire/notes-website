bfb169d5b77fb47eed9e75d67561102444c6f74b
jayeshhire <jayeshhire96@gmail.com>
2024-06-28 15:06:39 +0530

    added some abstraction

--- notes_site/settings.py
+++ notes_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from . import secret
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ihza0@)_yfmz457gxpo(a-$8!-n5fs)2#lv7$_#@$nh=s9(f('
+SECRET_KEY = secret.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
